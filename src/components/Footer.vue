08fe9412e44651912880277069ee0fe3e9179d22
Ahnaf Ahamed <ahnafahamed4@gmail.com>
2021-06-19 10:35:55 +0530

    completed

---
+++ src/components/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <footer>
+    <p>Copyrights &copy</p>
+    <router-link to="/about">About</router-link>
+  </footer>
+</template>
+
+<style scoped>
+a {
+  color: gray;
+}
+footer {
+  margin-top: 30px;
+  text-align: center;
+}
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,4 @@
+<template>
+  <h3>Version 0.1</h3>
+  <router-link to="/">Back to home</router-link>
+</template>
